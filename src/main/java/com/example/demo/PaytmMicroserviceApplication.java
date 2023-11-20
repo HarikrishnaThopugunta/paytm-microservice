45788d4b3199707d9b11a08b676f8c75fbf9df8c
HarikrishnaThopugunta <harikrishnathopugunta123@gmail.com>
2023-11-20 15:42:53 +0000

    second commit

--- src/main/java/com/example/demo/PaytmMicroserviceApplication.java
+++ src/main/java/com/example/demo/PaytmMicroserviceApplication.java
@@ -9,5 +9,4 @@ public class PaytmMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaytmMicroserviceApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/demo/controller/PaytmController.java
+++ src/main/java/com/example/demo/controller/PaytmController.java
@@ -6,16 +6,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.RequestContext;
 
 @RestController
 public class PaytmController {
-
 	
+	@GetMapping("/")
+	public String healthCheck() {
+		return "Paytm Service {healthy:true}";
+	}
 	
 	@GetMapping("/paytm-train-list")
 	public ResponseEntity<Object[]> getTrainList(){
+		System.out.println("Request Received to Paytm App");
 		RestTemplate restTemplate = new RestTemplate();		
-		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("http://localhost:9090/getTrainList", Object[].class);
+		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("http://irctc-microservice:9090/get-train-list", Object[].class);
 		Object[] objects = responseEntity.getBody();
 		Arrays.asList(objects).forEach(System.out::println);	
 		return responseEntity;
