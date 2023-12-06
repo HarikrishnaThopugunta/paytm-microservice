2a6dac0f4f0cfd8f0f0c39368082372e7f1ce869
HarikrishnaThopugunta <harikrishnathopugunta123@gmail.com>
2023-12-06 08:10:10 +0000

    added resiliency to app

--- src/main/java/com/example/demo/controller/PaytmController.java
+++ src/main/java/com/example/demo/controller/PaytmController.java
@@ -3,31 +3,43 @@ package com.example.demo.controller;
 import java.util.Arrays;
 import java.util.Objects;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.support.RequestContext;
 
-@RestController
+import com.example.demo.resilience.PaytmCircuitBreakerRegistry;
+
+@RestController()	
 public class PaytmController {
 	
-	@GetMapping("/")
+	@Autowired
+	private PaytmCircuitBreakerRegistry paytmCircuitBreakerRegistry;
+	
+	@GetMapping("/paytm-microservice")
 	public String healthCheck() {
 		return "Paytm Service {healthy:true}";
 	}
 	
-	@GetMapping("/paytm-train-list")
+	@GetMapping("/paytm-microservice/paytm-train-list")
 	public ResponseEntity<Object[]> getTrainList(){
 		System.out.println("Request Received to Paytm App");
 		RestTemplate restTemplate = new RestTemplate();		
 		String env = System.getenv("IRCTC_MICROSERVICE_SERVICE_HOST");
-		env = Objects.nonNull(env)? env :"http://localhost";
+		env = Objects.nonNull(env)? env+"/irctc-microservice" :"http://localhost";
 		System.out.println("env==============================>" + env);		
 		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity(env +":9090/get-train-list", Object[].class);
 		Object[] objects = responseEntity.getBody();
 		Arrays.asList(objects).forEach(System.out::println);	
 		return responseEntity;
+	}
+	
+	@GetMapping("/paytm-microservice/paytm-train-list-with-resilience")
+	public ResponseEntity<Object[]> getTrainListWithResilienec(){
+		System.out.println("Request Received to Paytm App");
+		
+		return paytmCircuitBreakerRegistry.getPaytmTrainList();
 	}
 	
 }
---
+++ src/main/java/com/example/demo/resilience/PaytmCircuitBreakerRegistry.java
@@ -0,0 +1,59 @@
+package com.example.demo.resilience;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.concurrent.TimeoutException;
+import java.util.function.Supplier;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+
+@Service
+public class PaytmCircuitBreakerRegistry {
+
+	CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+		    .failureRateThreshold(50)
+		    .waitDurationInOpenState(Duration.ofMillis(1000))
+		    .permittedNumberOfCallsInHalfOpenState(2)
+		    .slidingWindowSize(2)
+		    .recordExceptions(IOException.class, TimeoutException.class)
+		    //.ignoreExceptions(BusinessException.class, OtherBusinessException.class)
+		    .build();
+	
+	// Create a CircuitBreakerRegistry with a custom global configuration
+	CircuitBreakerRegistry circuitBreakerRegistry =
+	  CircuitBreakerRegistry.of(circuitBreakerConfig);
+	
+	CircuitBreaker circuitBreaker = circuitBreakerRegistry
+			  .circuitBreaker("paytm-cricuit-breaker");
+	
+	private Object[] getTrainList() {
+		RestTemplate restTemplate = new RestTemplate();			
+		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("http://localhost:9090/get-train-list", Object[].class);
+		Object[] objects = responseEntity.getBody();
+		Arrays.asList(objects).forEach(System.out::println);
+		return objects;
+	}
+	
+	public ResponseEntity<Object[]> getPaytmTrainList() {
+		RestTemplate restTemplate = new RestTemplate();			
+		ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity("http://localhost:9090/get-train-list", Object[].class);
+		Object[] objects = responseEntity.getBody();
+		Arrays.asList(objects).forEach(System.out::println);	
+		
+		Supplier<Object[]> decoratedSupplier = CircuitBreaker
+			    .decorateSupplier(circuitBreaker, this::getTrainList);
+
+		Object[] result = circuitBreaker
+				  .executeSupplier(this::getTrainList);
+		return responseEntity;
+	}
+	
+	
+}
