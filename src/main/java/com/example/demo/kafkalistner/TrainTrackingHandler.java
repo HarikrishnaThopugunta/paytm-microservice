63a6fbefaa3b449f068450be4e3f7eeaf68ce49c
harikrishnathopugunta123@gmail.com <harikrishnathopugunta123@gmail.com>
2023-12-21 11:56:26 +0530

    khkuhkjh

--- src/main/java/com/example/demo/kafkalistner/TrainTrackingHandler.java
+++ src/main/java/com/example/demo/kafkalistner/TrainTrackingHandler.java
@@ -9,7 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class TrainTrackingHandler {
 
-	@KafkaListener(id = "paytmConsumerClient", topics = "17235", groupId = "paytm-consumer-group")
+	@KafkaListener(id = "paytmConsumerClient", topics = "17235", groupId = "paytm.consumer.group")
 	public void listner(String payload) {
 		System.out.println("Message Received " + payload);
 	}
